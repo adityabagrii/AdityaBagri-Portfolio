aac34348c7869ad86aa05462a20daab8631e2dc1
Aditya Bagri <aditya22029@iiitd.ac.in>
2024-05-31 17:34:27 +0530

    about files created

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './About.css'
+
+const About = ({about}) => {
+  return (
+    <div className={about ? 'main':'hidden'}>
+      
+    </div>
+  )
+}
+
+export default About
